b5649ebe68abfa3ac8a60b74b5b1157dd71638d5
ゆっくりフラン_ch <64126253+yukkurihuranch@users.noreply.github.com>
2021-12-31 18:25:43 +0900

    Add files via upload

---
+++ mc/status/assets/js/Config.js
@@ -0,0 +1,59 @@
+const Config = {
+/* START CONFIG */
+
+'server-logo': 'assets/img/logo.png',
+
+'server-ip': 'mc.yuhunet.ml',
+
+'server-status': {
+	'background-color': '#fff659',
+	'text-color': 'black',
+	'icon': 'assets/img/status.png',
+
+	'value': '{online} / {max} players / Version 1.7.x-1.17.x',
+	'copy-value': 'IP COPIED!'
+},
+
+
+'background': {
+	'type': 'image', // TYPES: IMAGE OR COLOR
+	'value': "assets/img/background.jpg",
+	'opacity': {
+		'color': 'black',
+		'value': 0.8
+	}
+},
+
+
+'buttons-style': {
+	'background-color': '#fff659',
+	'text-color': 'black'
+},
+
+'web-buttons': {
+	'JapanServers': {
+		icon: 'assets/img/1500x500.png',
+		url: 'https://minecraft.jp/servers/mc.yuhunet.ml'
+	},
+	'Twitter': {
+		icon: 'assets/img/Twitter-Logo-removebg-preview.png',
+		url: 'https://twitter.com/yuhurannetwork'
+	},
+	'Discord': {
+		icon: 'assets/img/Ko5FceZ4_400x400.jpg',
+		url: 'https://discord.gg/Q9gztwkkS8'
+	}
+}
+
+
+
+
+/* END CONFIG */
+};
+
+
+
+
+
+/* IGNORE */
+export default Config;
